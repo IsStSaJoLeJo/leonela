3b456a28aab0f4a3d2dfa5727eae40a57937d628
Karen <julieth.sanmiguel@espoch.edu.ec>
2024-11-25 16:05:08 -0500

    pedrito

---
+++ src/main/java/com/mycompany/paquete/dejesus/Dejesus.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.paquete.dejesus;
+
+/**
+ *
+ * @author DELL
+ */
+public class Dejesus {
+
+    public static void main(String[] args) {
+        System.out.println("Leonel");
+        System.out.println("y");
+        System.out.println("o");
+    }
+}
